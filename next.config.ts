c60dc4a8c60bbc5e63bee7a1032895b36ea97cf8
jeroennisser <jeroennisser@gmail.com>
2025-11-28 18:38:56 +0100

    jinacms

--- next.config.ts
+++ next.config.ts
@@ -34,14 +34,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: '/admin',
-        destination: '/admin/index.html',
-      },
-    ];
-  },
 };
 
 export default nextConfig
